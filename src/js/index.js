bfa71077b91ac3665849c825922871275c497648
SevroAuBarca <daniel_contreras001@hotmail.com>
2022-05-08 15:42:56 -0500

    terminando parte desktop

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,23 @@
 const exitMenu = document.querySelector(".header__navbar__exit");
 const openMenu = document.querySelector(".header__hamburguer");
 const navBar = document.querySelector(".header__navbar");
-
+const arrow = [...document.querySelectorAll(".details__summary")];
 openMenu.addEventListener("click", () => {
   navBar.classList.toggle("open");
 });
 
 exitMenu.addEventListener("click", () => {
   navBar.classList.toggle("open");
+});
+
+arrow.forEach((el) => {
+  el.addEventListener("click", () => {
+    if (el.classList.contains("down")) {
+      el.classList.remove("down");
+      el.classList.add("up");
+    } else {
+      el.classList.remove("up");
+      el.classList.add("down");
+    }
+  });
 });
